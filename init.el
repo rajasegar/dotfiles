c4a6a34229ff63c52e67706a862bf92159a24032
Rajasegar <rajasegar.c@gmail.com>
2022-07-30 12:48:12 +0530

    chore: add slime config back
    1. disable line numbers for slime mode
    2. sync mac init.el file

--- init.el
+++ init.el
@@ -4,10 +4,11 @@
 ;; - https://dev.to/huytd/emacs-from-scratch-1cg6
 ;; - https://sam217pa.github.io/2016/09/13/from-helm-to-ivy/#fnref:2
 
+;;; Code:
 (setq make-backup-files nil) ; stop creating backup~ files
 (setq auto-save-default nil) ; stop creating #autosave# files
 (setq delete-old-versions -1 )
-; (setq inhibit-startup-screen t )
+(setq inhibit-startup-screen t )
 (setq ring-bell-function 'ignore )
 (setq coding-system-for-read 'utf-8 )
 (setq coding-system-for-write 'utf-8 )
@@ -15,6 +16,17 @@
 (setq default-fill-column 80)
 (setq initial-scratch-message "")
 (setq word-wrap t)
+
+;; Start full screen and maximized
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+;; Tab settings
+(setq indent-tabs-mode nil)
+(setq evil-indent-convert-tabs nil)
+(setq tab-width 2)
+
+;; Enable copy paste
+(setq x-select-enable-clipboard t)
 
 ;; https://github.com/danielmai/.emacs.d/blob/master/config.org
 (defalias 'yes-or-no-p 'y-or-n-p)
@@ -30,6 +42,11 @@
 
 ;; Set relative line numbers
 (setq display-line-numbers-type 'relative)
+
+;; Disable line numbers for some modes
+(dolist (mode '(term-mode-hook
+                eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 ;; Package Management
 (require 'package)
@@ -73,6 +90,40 @@
   :config
   (load-theme 'doom-dracula t))
 
+;; Neotree
+(use-package neotree
+  :ensure t
+  :config
+  ;; Disable line-numbers minor mode for neotree
+  (add-hook 'neo-after-create-hook
+            (lambda (&rest _) (display-line-numbers-mode -1))))
+(global-set-key [f8] 'neotree-toggle)
+
+(defun neotree-project-dir ()
+    "Open NeoTree using the git root."
+    (interactive)
+    (let ((project-dir (projectile-project-root))
+          (file-name (buffer-file-name)))
+      (neotree-toggle)
+      (if project-dir
+          (if (neo-global--window-exists-p)
+              (progn
+                (neotree-dir project-dir)
+                (neotree-find file-name)))
+        (message "Could not find git project root."))))
+
+(global-set-key [f8] 'neotree-project-dir)
+
+;; Ignore files in neotree
+(setq neo-hidden-regexp-list
+    '("^\\." "\\.pyc$" "~$" "^#.*#$" "\\.elc$" "\\.o$" ;; defaults
+      ;; add yours:
+      "node_modules"))
+
+;; Icons for netore
+(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+
+
 ;; Which Key
 (use-package which-key
   :ensure t
@@ -86,14 +137,9 @@
   :ensure t
   :init (global-flycheck-mode))
 
-;; (use-package lsp-mode
-;;   :ensure t
-;;   :init
-;;   (setq lsp-auto-guess-root t)
-;;   (setq lsp-prefer-flymake nil)
-;;   :config
-;;   (add-hook 'js2-mode-hook #'lsp)
-;;   (add-hook 'rjsx-mode-hook #'lsp))
+;; org load languages
+(org-babel-do-load-languages 'org-babel-load-languages
+    '((shell . t)))
 
 ;; (use-package lsp-ui
 ;;   :ensure t
@@ -112,19 +158,66 @@
 ;;   	lsp-ui-peek-peek-height 25)
 ;;   (add-hook 'lsp-mode-hook 'lsp-ui-mode))
 
-;; (use-package company-lsp
-;;   :ensure t
-;;   :commands company-lsp)
+;; Org mode enhancements
+(use-package org-bullets
+  :ensure t
+  :init (org-mode))
+
+;; Powerline
+(use-package powerline
+  :ensure t
+  :config
+    (powerline-default-theme))
+(use-package airline-themes
+  :ensure t
+  :config
+    (load-theme 'airline-dark t))
 
 (use-package company
   :ensure t
   :init
   (add-hook 'after-init-hook 'global-company-mode))
 
-;; Elixir
-(use-package elixir-mode
-  :ensure t)
+;; yaml
+(use-package yaml-mode
+  :ensure t
+  :init
+  (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode)))
 
+;; json
+(use-package json-mode
+  :ensure t
+  :init
+  (add-to-list 'auto-mode-alist '("\\.json\\'" . json-mode)))
+
+;; ;; epub
+;; (use-package nov
+;;   :ensure t
+;;   :init
+;;   (add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode)))
+
+;; ;; visual-fill-column for epub files
+;; (use-package visual-fill-column
+;;   :ensure t
+;;   :init
+;;   (add-hook 'visual-line-mode-hook #'visual-fill-column-mode))
+
+(setq nov-text-width t)
+(setq visual-fill-column-center-text t)
+(add-hook 'nov-mode-hook 'visual-line-mode)
+(add-hook 'nov-mode-hook 'visual-fill-column-mode)
+
+;; Skewer
+;; (use-package simple-httpd
+;;   :ensure t
+;;   :init
+;;   (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode)))
+
+;; (use-package skewer-mode
+;;   :ensure t
+;;   :init
+;;   (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode)))
+  
 ;; Javascript
 (use-package js2-mode 
   :ensure t
@@ -133,9 +226,26 @@
 (use-package rjsx-mode
   :ensure t
   :init
-  (add-to-list 'auto-mode-alist '("components\\/.*\\.js\\'" . rjsx-mode)))
+  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.svelte\\'" . web-mode)))
+
+;; LSP
+(use-package lsp-mode
+  :ensure t
+  :hook (
+	 (js2-mode . lsp-deferred)
+	 (typescript-mode . lsp-deferred)
+	 (web-mode . lsp-deferred)
+	 (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp-deferred)
+;; LSP performance
+(setq gc-cons-threshold 100000000)
+(setq read-process-output-max (* 1024 1024)) ;; 1mb
+(setq lsp-disabled-clients '(eslint))
 (use-package add-node-modules-path
   :ensure t)
+
 (use-package prettier-js
   :ensure t
   :init
@@ -144,6 +254,19 @@
   (add-hook 'js2-mode-hook 'prettier-js-mode)
   (add-hook 'web-mode-hook 'add-node-modules-path)
   (add-hook 'web-mode-hook 'prettier-js-mode))
+
+;; Slime
+(use-package slime
+  :ensure t)
+(setq inferior-lisp-program "sbcl")
+
+;; Mac OSX owns C-up and C-down, so arrange for history
+;; navigation to do something useful via C-c p and C-c n.
+(eval-after-load 'slime
+  `(progn
+     (define-key slime-prefix-map "p" 'slime-repl-backward-input)
+     (define-key slime-prefix-map "n" 'slime-reply-forward-input)
+		 (global-display-line-numbers-mode 0)))
 
 ;; Markdown
 (use-package markdown-mode
