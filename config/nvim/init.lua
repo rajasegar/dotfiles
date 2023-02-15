db1d62225a584c1449161eb046ca545e69143960
Rajasegar <rajasegar.c@gmail.com>
2023-02-15 10:41:04 +0530

    chore: moving coc config to new file

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -4,6 +4,7 @@ require('keymaps')
 require('autocmd')
 require('nerdtree')
 require('linting')
+require('coc-options')
 
 vim.cmd([[
 
@@ -42,31 +43,4 @@ let g:vim_markdown_conceal = 0
 " Disable quote concealing in JSON files
 let g:vim_json_conceal=0
 
-" CoC extensions
-let g:coc_global_extensions = ['coc-tsserver', 'coc-html', 'coc-css']
-
-" Use tab for trigger completion with characters ahead and navigate.
-" NOTE: Use command ':verbose imap <tab>' to make sure tab is not mapped by
-" other plugin before putting this into your config.
-inoremap <silent><expr> <TAB>
-      \ pumvisible() ? "\<C-n>" :
-      \ CheckBackspace() ? "\<TAB>" :
-      \ coc#refresh()
-inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
-
-function! CheckBackspace() abort
-  let col = col('.') - 1
-  return !col || getline('.')[col - 1]  =~# '\s'
-endfunction
-
-" Use `[g` and `]g` to navigate diagnostics
-" Use `:CocDiagnostics` to get all diagnostics of current buffer in location list.
-nmap <silent> [g <Plug>(coc-diagnostic-prev)
-nmap <silent> ]g <Plug>(coc-diagnostic-next)
-
-" GoTo code navigation.
-nmap <silent> gd <Plug>(coc-definition)
-nmap <silent> gy <Plug>(coc-type-definition)
-nmap <silent> gi <Plug>(coc-implementation)
-nmap <silent> gr <Plug>(coc-references)
 ]])
