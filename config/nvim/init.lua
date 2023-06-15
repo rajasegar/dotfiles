e3db9ec61999b499e52f2bef6269aa3af101dfca
Rajasegar <rajasegar.c@gmail.com>
2023-06-15 18:04:58 +0530

    fix: treesitter parser issue

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -45,7 +45,7 @@ require('lazy').setup({
   { 
     'nvim-treesitter/nvim-treesitter',
     config = function()
-      -- pcall(require('nvim-treesitter.install').update { with_sync = true })
+      pcall(require('nvim-treesitter.install').update { with_sync = true })
     end,
   },
 
