e9b13e8c3d89f8c2b04987da377a90978b604cca
Rajasegar <rajasegar.c@gmail.com>
2023-09-19 14:08:15 +0530

    chore: map F12 to persp-next

--- emacs.d/rajasegar/keybindings.el
+++ emacs.d/rajasegar/keybindings.el
@@ -1,11 +1,11 @@
 ;; Keybindings
 
 (global-set-key [f5] 'emms-start)
-(global-set-key [f6] 'emms-pause)
+(global-set-key [f6] 'emms-stop)
 (global-set-key [f7] 'emms-previous)
 (global-set-key [f8] 'emms-next)
 (global-set-key [f9] 'neotree-show)
-;; (global-set-key [f12] ')
+(global-set-key [f12] 'persp-next)
 
 (use-package general
   :ensure t
