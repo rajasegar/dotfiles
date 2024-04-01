8671a6eebc3f0348ba2c0ce94091f7914e8beebe
Rajasegar <rajasegar.c@gmail.com>
2024-04-01 15:55:17 +0530

    chore: remove general package dependencies
    Use native global-set-key for keybinding

--- emacs.d/rajasegar/keybindings.el
+++ emacs.d/rajasegar/keybindings.el
@@ -17,212 +17,147 @@
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
 (global-set-key (kbd "M-x") 'counsel-M-x)
 
-(use-package general
-  :ensure t
-  :config
+;; Applications
+(global-set-key (kbd "C-c a c") 'calendar)
+(global-set-key (kbd "C-c a d") 'counsel-dired)
+(global-set-key (kbd "C-c a m") 'emms)
+(global-set-key (kbd "C-c a k") 'calc)
+(global-set-key (kbd "C-c a h") 'rajasegar/open-hackernews)
+(global-set-key (kbd "C-c a p") 'prodigy)
+(global-set-key (kbd "C-c a r") 'elfeed)
+(global-set-key (kbd "C-c a t") 'eshell-extensions/open-new-eshell)
+(global-set-key (kbd "C-c a w") 'rajasegar/update-wallpaper)
 
-  (general-define-key
-   :states '(normal visual)
-   "C-u" 'scroll-down-command
-   "C-d" 'scroll-up-command)
+;; Buffers
+(global-set-key (kbd "C-c b d") 'kill-this-buffer)
+(global-set-key (kbd "C-c b e") 'eval-buffer)
+(global-set-key (kbd "C-c b h") 'rajasegar/switch-to-dashboard)
+(global-set-key (kbd "C-c b n") 'centaur-tabs-forward)
+(global-set-key (kbd "C-c b p") 'centaur-tabs-backward)
+(global-set-key (kbd "C-c b r") 'rename-buffer)
+(global-set-key (kbd "C-c b s") 'scratch-buffer)
+(global-set-key (kbd "C-c b e") 'erase-buffer)
 
-  (general-define-key
-   :states '(normal visual emacs)
-   :prefix "SPC"
-   :non-normal-prefix "C-SPC"
-   "'"   'multi-term
-   "/"   'counsel-rg
-   "SPC"   'counsel-M-x
-   "."   'rajasegar/edit-emacs-configuration
-   ";"   'emms-pause
-   "\""  'split-window-below
-   ","  'query-replace
-   "TAB" 'rajasegar/toggle-buffers
+
+(global-set-key (kbd "C-c c g") 'counsel-rg)
+
+
+(global-set-key (kbd "C-c d f") 'js-doc-insert-function-doc)
+(global-set-key (kbd "C-c d m") 'js-doc-insert-file-doc)
+
+;; Files and Flymake
+(global-set-key (kbd "C-c f r") 'counsel-recentf)
+(global-set-key (kbd "C-c f n") 'flymake-goto-next-error)
+(global-set-key (kbd "C-c f d") 'flymake-show-buffer-diagnostics)
    
-   "T" 'counsel-load-theme
+;; Git
+(global-set-key (kbd "C-c g a") 'github-approve-pr)
+(global-set-key (kbd "C-c g b") 'magit-blame)
+(global-set-key (kbd "C-c g c") 'github-approve-current-pr)
+(global-set-key (kbd "C-c g f") 'rajasegar/stage-file-in-current-line)
+(global-set-key (kbd "C-c g g") 'rajasegar/create-gist)
+(global-set-key (kbd "C-c g h") 'rajasegar/switch-git-personal)
+(global-set-key (kbd "C-c g i") 'rajasegar/open-github-issues)
+(global-set-key (kbd "C-c g l") 'github-list-pr-files)
+(global-set-key (kbd "C-c g m") 'github-merge-pr)
+(global-set-key (kbd "C-c g n") 'rajasegar/open-new-pull-request)
+(global-set-key (kbd "C-c g o") 'rajasegar/open-project-in-github)
+(global-set-key (kbd "C-c g p") 'magit-push-current-to-upstream)
+(global-set-key (kbd "C-c g r") 'rajasegar/git-reverse-merge-dev-branch)
+(global-set-key (kbd "C-c g s") 'magit-status)
+(global-set-key (kbd "C-c g t") 'git-timemachine)
+(global-set-key (kbd "C-c g w") 'rajasegar/switch-git-work)
+(global-set-key (kbd "C-c g x") 'github-prs-repo)
+(global-set-key (kbd "C-c g z") 'rajasegar/magit-stash-untracked)
 
-   "a" '(:ignore t :which-key "Applications")
-   "ac" 'calendar
-   "ad" 'counsel-dired
-   "am" 'emms
-   "ak" 'calc
-   "ah" 'rajasegar/open-hackernews
-   "ap" 'prodigy
-   "ar" 'elfeed
-   "at" 'eshell-extensions/open-new-eshell
-   "aw" 'rajasegar/update-wallpaper
 
 
-   "b" '(:ignore t :which-key "Buffers")
-   "bb"  'ivy-switch-buffer
-   "bd" 'kill-this-buffer
-   "be" 'eval-buffer
-   "bh" 'rajasegar/switch-to-dashboard
-   "bn" 'centaur-tabs-forward
-   "bp" 'centaur-tabs-backward
-   "br" 'rename-buffer
-   "bs" 'scratch-buffer
-   "bx" 'erase-buffer
+(global-set-key (kbd "C-c i") 'ielm)
 
-   "c" '(:ignore t :which-key "Comment")
-   "cb" 'counsel-bookmark
-   "cl" 'comment-line
-   "cg" 'counsel-rg
 
-   "d" '(:ignore t :which-key "Comments")
-   "df" 'js-doc-insert-function-doc
-   "dm" 'js-doc-insert-file-doc
+;; Jump 
+(global-set-key (kbd "C-c j j") 'switch-to-buffer)
+(global-set-key (kbd "C-c j h") 'switch-to-prev-buffer)
+(global-set-key (kbd "C-c j l") 'switch-to-next-buffer)
+(global-set-key (kbd "C-c j c") 'rajasegar/jump-to-component)
+(global-set-key (kbd "C-c j t") 'rajasegar/jump-to-template)
 
-   "e" '(:ignore t :which-key "Edit Configs")
-   "e." 'rajasegar/open-emacs-config-folder
-   "ef" 'rajasegar/edit-emacs-functions
-   "ek" 'rajasegar/edit-emacs-keybindings
-   "ep" 'rajasegar/edit-emacs-packages
-   "er" 'rajasegar/run-rcup
-   "es" 'rajasegar/edit-emacs-settings
+(global-set-key (kbd "C-c k") 'eval-expression)
 
-   "f" '(:ignore t :which-key "Files")
-   "ff" 'counsel-find-file
-   "fr" 'counsel-recentf
-   "fs" 'save-buffer
-   
-   ;; Flymake
-   "fn" 'flymake-goto-next-error
-   ;; "fp" 'flymake-goto-prev-error
-   "fd" 'flymake-show-buffer-diagnostics
-   
+;; Eglot
+(global-set-key (kbd "C-c l i") 'eglot-find-implementation)
+(global-set-key (kbd "C-c l t") 'eglot-find-typeDefinition)
+(global-set-key (kbd "C-c l d") 'eglot-find-declaration)
 
-   "g" '(:ignore t :which-key "Git")
-   "gb" 'magit-blame
-   "gc" 'rajasegar/compare-git-branches
-   "gf" 'rajasegar/stage-file-in-current-line
-   "gg" 'rajasegar/create-gist
-   "gh" 'rajasegar/switch-git-personal
-   "gi" 'rajasegar/open-github-issues
-   "gm" 'rajasegar/open-github-pull-requests
-   "gn" 'rajasegar/open-new-pull-request
-   "go" 'rajasegar/open-project-in-github
-   "gp" 'magit-push-current-to-upstream
-   "gs" 'magit-status
-   "gt" 'git-timemachine
-   "gw" 'rajasegar/switch-git-work
-   "gz" 'rajasegar/magit-stash-untracked
+;; Emms
+(global-set-key (kbd "C-c m b") 'emms-browser)
+(global-set-key (kbd "C-c m d") 'emms-play-directory-tree)
+(global-set-key (kbd "C-c m n") 'emms-next)
+(global-set-key (kbd "C-c m o") 'emms-show)
+(global-set-key (kbd "C-c m p") 'emms-previous)
+(global-set-key (kbd "C-c m s") 'emms-shuffle)
+(global-set-key (kbd "C-c m x") 'emms-pause)
+(global-set-key (kbd "C-c m f") 'rajasegar/play-favs-folder)
+(global-set-key (kbd "C-c m c") 'rajasegar/play-college-folder)
+(global-set-key (kbd "C-c m l") 'rajasegar/play-latest-folder)
 
-   "h" '(:ignore t :which-key "Help")
-   "hb" 'counsel-descbinds
-   "hf" 'counsel-describe-function
-   "hv" 'counsel-describe-variable
+;; Org mode
+(global-set-key (kbd "C-c o a") 'org-agenda)
+(global-set-key (kbd "C-c o c") 'org-capture)
+(global-set-key (kbd "C-c o p") 'org-present)
+(global-set-key (kbd "C-c o t") 'org-toggle-link-display)
+(global-set-key (kbd "C-c o s") 'org-timer-set-timer)
+(global-set-key (kbd "C-c o x") 'org-timer-stop)
 
-   "i" 'ielm
+;; Projects
+(global-set-key (kbd "C-c p f") 'counsel-projectile-find-file)
+(global-set-key (kbd "C-c p p") 'rajasegar/find-projects)
+(global-set-key (kbd "C-c p t") 'neotree-show)
 
-   "j" '(:ignore t :which-key "Jump-Buffer")
-   "jj" 'switch-to-buffer
-   "jh" 'switch-to-prev-buffer
-   "jl" 'switch-to-next-buffer
-   "jc" 'rajasegar/jump-to-component
-   "jt" 'rajasegar/jump-to-template
+;; Quit
+(global-set-key (kbd "C-c q q") 'kill-emacs)
+(global-set-key (kbd "C-c q r") 'restart-emacs)
 
-   "k" 'eval-expression
 
-   "l" '(:ignore t :whick-key "LSP")
-   "li" 'eglot-find-implementation
-   "lt" 'eglot-find-typeDefinition
-   "ld" 'eglot-find-declaration
-
-   "m" '(:ignore t :which-key "Music")
-   "mb" 'emms-browser
-   "md" 'emms-play-directory-tree
-   "mn" 'emms-next
-   "mo" 'emms-show
-   "mp" 'emms-previous
-   "ms" 'emms-shuffle
-   "mx" 'emms-stop
-   "m1" 'rajasegar/play-favs-folder
-   "m2" 'rajasegar/play-college-folder
-   "m3" 'rajasegar/play-latest-folder
+;; Slime
+(global-set-key (kbd "C-c s s") 'slime)
+(global-set-key (kbd "C-c s l") 'slime-load-file)
+(global-set-key (kbd "C-c s w") 'rajasegar/counsel-rg-word)
    
 
-   "o" '(:ignore t :which-key "Org-Mode")
-   "oa" 'org-agenda
-   "oc" 'org-capture
-   "op" 'org-present
-   "ot" 'org-toggle-link-display
-   "os" 'org-timer-set-timer
-   "ox" 'org-timer-stop
+;; Tabs and tree sitter
+(global-set-key (kbd "C-c t c") 'rajasegar/add-codeium-completions)
+(global-set-key (kbd "C-c t e") 'treesit-explore-mode)
+(global-set-key (kbd "C-c t l") 'toggle-truncate-lines)
+(global-set-key (kbd "C-c t n") 'tab-bar-new-tab)
+(global-set-key (kbd "C-c t p") 'tab-bar-switch-to-prev-tab)
+(global-set-key (kbd "C-c t q") 'tree-sitter-query-builder)
 
-   "p" 'projectile-command-map
-   "pf" 'counsel-projectile-find-file
-   "pp" 'rajasegar/find-projects
-   ;; "pp" 'projectile-switch-project
-   "pt" 'neotree-show
+;; Eshell 
+(global-set-key (kbd "C-c v c") 'eshell-extensions/eshell-command-current-line)
+(global-set-key (kbd "C-c v r") 'eshell-command)
+(global-set-key (kbd "C-c v l") 'eshell-extensions/run-previous-eshell-command)
+(global-set-key (kbd "C-c v k") 'eshell-extensions/kill-buffer-eshell-command-output)
+(global-set-key (kbd "C-c v v") 'eshell-extensions/eshell-vertical)
+(global-set-key (kbd "C-c v h") 'eshell-extensions/eshell-horizontal)
 
-   "qq" 'kill-emacs
-   "qr" 'restart-emacs
 
-   "s" '(:ignore t :which-key "Slime")
-   "ss" 'slime
-   "sl" 'slime-load-file
-   "sw" 'rajasegar/counsel-rg-word
+(global-set-key (kbd "C-c x e") 'rajasegar/eval-print-last-sexp-no-truncation)
+(global-set-key (kbd "C-c x p") 'rajasegar/create-prodigy-service)
 
-   "t" '(:ignore t :which-key "Tabs & Toggles")
-   "tc" 'rajasegar/add-codeium-completions
-   "te" 'treesit-explore-mode
-   "tl" 'toggle-truncate-lines
-   "tn" 'tab-bar-new-tab
-   "tp" 'tab-bar-switch-to-prev-tab
-   "tq" 'tree-sitter-query-builder
+;; Yasnippets
+(global-set-key (kbd "C-c y n") 'yas-new-snippet)
+(global-set-key (kbd "C-c y i") 'yas-insert-snippet)
 
-   "v" '(:ignore t :which-key "Eshell")
-   "vc" 'eshell-extensions/eshell-command-current-line
-   "vr" 'eshell-command
-   "vl" 'eshell-extensions/run-previous-eshell-command
-   "vk" 'eshell-extensions/kill-buffer-eshell-command-output
-   "vv" 'eshell-extensions/eshell-vertical
-   "vh" 'eshell-extensions/eshell-horizontal
+;; Freddy
+(global-set-key (kbd "C-c ` p") 'freddy-ai/prompt)
+(global-set-key (kbd "C-c ` r") 'freddy-ai/rephrase-from-region)
+(global-set-key (kbd "C-c ` l") 'freddy-ai/prompt-from-line)
+(global-set-key (kbd "C-c ` c") 'freddy-ai/write-code)
+(global-set-key (kbd "C-c ` t") 'freddy-ai/write-tests)
 
-   "w" '(:ignore t :which-key "Window")
-   "w-"  'split-window-below
-   "w/"  'split-window-right
-   "wd"  'delete-window
-   "wh"  'windmove-left
-   "wj"  'windmove-down
-   "wk"  'windmove-up
-   "wl"  'windmove-right
-   "ww"  'evil-window-next
 
-   "x" '(:ignore t :which-key "Execute")
-   "xe" 'rajasegar/eval-print-last-sexp-no-truncation
-   "xl" 'eval-last-sexp
-   "xp" 'rajasegar/create-prodigy-service
 
-   "y" '(:ignore t :which-key "Yasnippet")
-   "yn" 'yas-new-snippet
-   "yi" 'yas-insert-snippet
-
-   "`" '(:ignore t :which-key "Freddy AI")
-   "`p" 'freddy-ai/prompt
-   "`r" 'freddy-ai/rephrase-from-region
-   "`l" 'freddy-ai/prompt-from-line
-   "`c" 'freddy-ai/write-code
-   "`t" 'freddy-ai/write-tests
-   )
-
-  (general-define-key
-   :states '(normal visual)
-   :keymaps 'dired-mode-map
-   "p" 'emms-play-dired
-   "S" 'rajasegar/dired-sort-size
-   "s" 'rajasegar/dired-sort-size-reverse
-   )
-
-  (general-define-key
-   :states '(visual)
-   "gc" 'evilnc-comment-or-uncomment-lines)
-
-  (general-define-key
-   :states '(normal visual)
-   :keymaps 'neotree-mode-map
-   "o" 'neotree-quick-look))
 
 (provide 'keybindings)
 
