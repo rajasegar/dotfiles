5c9a4204a1acd1e18e20e358dec813f6e1996f94
Rajasegar <rajasegar.c@gmail.com>
2024-08-10 07:11:09 +0530

    chore: put up a working eglot config for ts and js

--- emacs.d/rajasegar/eglot-config.el
+++ emacs.d/rajasegar/eglot-config.el
@@ -1,17 +1,34 @@
 ;; LSP with eglot
 
+(add-to-list 'auto-mode-alist '("\\.ts?\\'" . typescript-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.tsx?\\'" . typescript-ts-mode))
+
+(put 'typescript-ts-mode 'eglot-language-id "typescriptreact")
+(put 'js-mode 'eglot-language-id "javascript")
+
 (add-hook 'js-mode-hook 'eglot-ensure)
 (add-hook 'css-mode-hook 'eglot-ensure)
 (add-hook 'html-mode-hook 'eglot-ensure)
-(add-hook 'typescript-mode-hook 'eglot-ensure)
+(add-hook 'typescript-ts-mode-hook 'eglot-ensure)
 (add-hook 'hbs-mode-hook 'eglot-ensure)
+(add-hook 'c-mode-hook 'eglot-ensure)
+(add-hook 'web-mode-hook 'eglot-ensure)
+
+(defun jsonrpc--log-event (connection message &optional type))
 
 ;; Handlebars mode
 (define-derived-mode hbs-mode web-mode "Handlebars mode" "Major mode for handlebars")
 (add-to-list 'auto-mode-alist '("\\.hbs\\'" . hbs-mode))
 
-
 (with-eval-after-load 'eglot
+;; No event buffers, disable providers cause a lot of hover traffic. Shutdown unused servers.
+  (setq eglot-events-buffer-size 0
+        ;; eglot-ignored-server-capabilities '(
+                                            ;; :hoverProvider
+                                            ;; :documentHighlightProvider
+                                            ;; :inlayHintProvider)
+        eglot-autoshutdown t)
+
   (add-to-list 'eglot-server-programs
                (if (string-equal system-type "darwin")
                    '(hbs-mode . ("node" "/Users/rajasegarchandran/www/ember-language-server/lib/start-server.js" "--stdio"))
@@ -19,7 +36,5 @@
                ;; '(js2-mode . ("node" "/home/boot/Public/www/bun-lsp/out/server.js" "--stdio"))
                ))
 
-(add-hook 'eglot-managed-mode-hook (lambda ()
-                                     (add-to-list 'company-backends
-                                                  '(company-capf :with company-yasnippet))))
+
 
